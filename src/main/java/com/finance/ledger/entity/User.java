6fac681527992100ea0eb85fdcde09b16247a676
taejeong <cyc08071@naver.com>
2026-05-26 00:00:35 +0900

    feat: initialize spring boot project and add user entity

---
+++ src/main/java/com/finance/ledger/entity/User.java
@@ -0,0 +1,22 @@
+package com.finance.ledger.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+}
---
+++ src/test/java/com/finance/ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
